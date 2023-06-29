031733ed725aaa27e0759d2e62c3a6179a4cbaa7
cv1llanueva <134245641+cv1llanueva@users.noreply.github.com>
2023-06-29 01:31:36 -0500

    prueba de ultimos algortimos implementados

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Graph/graph.h"
 #include <iostream>
-
+#include "kruskal.h"
+#include "bfs.h"
 using namespace std;
 
 int main() {
@@ -15,29 +16,39 @@ int main() {
     graph.displayVertex("2796");
     graph.display();
     // density
-    cout << "Density: " << graph.density() << endl;
-    cout << "Is dense: " << graph.isDense() << endl;
-    // Operator
-    cout << "Operator: " << graph("2792","2796") << endl;
-    // Find by ID
-    cout << "Find by ID: " << graph.findById("2792") << endl;
-    // delete edge
-    cout << "Deleting edge" << endl;
-    graph.deleteEdge("2792", "2796");
-    graph.displayVertex("2792");
-    graph.displayVertex("2796");
-    graph.display();
-    // delete vertex
-    cout << "Deleting vertex" << endl;
-    graph.deleteVertex("2792");
-    graph.displayVertex("2792");
-    graph.displayVertex("2796");
-    graph.display();
-    // clear graph
-    cout << "Clearing graph" << endl;
-    graph.clear();
-    graph.display();
+    // cout << "Density: " << graph.density() << endl;
+    // cout << "Is dense: " << graph.isDense() << endl;
+    // // Operator
+    // cout << "Operator: " << graph("2792","2796") << endl;
+    // // Find by ID
+    // cout << "Find by ID: " << graph.findById("2792") << endl;
+    // // delete edge
+    // cout << "Deleting edge" << endl;
+    // graph.deleteEdge("2792", "2796");
+    // graph.displayVertex("2792");
+    // graph.displayVertex("2796");
+    // graph.display();
+    // // delete vertex
+    // cout << "Deleting vertex" << endl;
+    // graph.deleteVertex("2792");
+    // graph.displayVertex("2792");
+    // graph.displayVertex("2796");
+    // graph.display();
+    // // clear graph
+    // cout << "Clearing graph" << endl;
+    // graph.clear();
+    // graph.display();
+    
+    // Realizar la búsqueda BFS desde el vértice "A"
+    cout << "BFS busqueda desde A:" << endl;
+    BFS(graph, "2792");
+    cout << endl;
+    vector<Edge<string, float>*> result = exec_kruskal<string, float>(graph);
 
+    cout << "Aristas del MST:" << endl;
+    for (auto edge : result) {
+        cout << edge->vertexes[0]->data << " - " << edge->vertexes[1]->data << ", peso: " << edge->weight << endl;
+    }
 
     return 0;
 }
