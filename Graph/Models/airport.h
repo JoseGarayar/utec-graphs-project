58e1c6fcdbb0216060c36b42231a8e94b44fe11d
Juan José Granados <juanjogramo@gmail.com>
2023-07-09 16:14:50 -0500

    feat(parser): include nlohmann library to parse json

---
+++ Graph/Models/airport.h
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#ifndef AIRPORT_H
+#define AIRPORT_H
+
+struct Airport {
+    string id;
+    string city;
+    string country;
+    string latitude;
+    string longitude;
+    string name;
+};
+
+#endif
