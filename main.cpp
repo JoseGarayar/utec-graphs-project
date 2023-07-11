e574a009632013c26cb160d34a3f764b452f2669
Jose Garayar <jgarayarp@uni.pe>
2023-07-11 02:42:30 -0500

    test(algorithm): Refactor test for graph
    Move graph methods tests to tester file

--- main.cpp
+++ main.cpp
@@ -12,40 +12,9 @@ using namespace std;
 int main() {
     GraphFactory factory;
     Graph<Airport, float> gr = factory.makeGraph();
-    
-    Graph<string,float> graph;
-    // Testing graph methods
-    // create vertex
-    graph.insertVertex("2792", "Juliaca");
-    graph.insertVertex("2796", "Tumbes");
-    // create edge
-    graph.createEdge("2792", "2796", 400);
-    // graph.displayVertex("2792");
-    // graph.displayVertex("2796");
-    // graph.display();
-    // density
-    // cout << "Density: " << graph.density() << endl;
-    // cout << "Is dense: " << graph.isDense() << endl;
-    // // Operator
-    // cout << "Operator: " << graph("2792","2796") << endl;
-    // // Find by ID
-    // cout << "Find by ID: " << graph.findById("2792") << endl;
-    // // delete edge
-    // cout << "Deleting edge" << endl;
-    // graph.deleteEdge("2792", "2796");
-    // graph.displayVertex("2792");
-    // graph.displayVertex("2796");
-    // graph.display();
-    // // delete vertex
-    // cout << "Deleting vertex" << endl;
-    // graph.deleteVertex("2792");
-    // graph.displayVertex("2792");
-    // graph.displayVertex("2796");
-    // graph.display();
-    // // clear graph
-    // cout << "Clearing graph" << endl;
-    // graph.clear();
-    // graph.display();
+
+    // Test graph methods
+    test_graph_methods();
 
     // Test A*
     test_astar();
--- tester.h
+++ tester.h
@@ -8,6 +8,42 @@
 
 using namespace std;
 
+void test_graph_methods() {
+    Graph<string,float> graph;
+    // Testing graph methods
+    // create vertex
+    graph.insertVertex("2792", "Juliaca");
+    graph.insertVertex("2796", "Tumbes");
+    // create edge
+    graph.createEdge("2792", "2796", 400);
+    graph.displayVertex("2792");
+    graph.displayVertex("2796");
+    graph.display();
+    // density
+    cout << "Density: " << graph.density() << endl;
+    cout << "Is dense: " << graph.isDense() << endl;
+    // // Operator
+    cout << "Operator: " << graph("2792","2796") << endl;
+    // // Find by ID
+    cout << "Find by ID: " << graph.findById("2792") << endl;
+    // // delete edge
+    cout << "Deleting edge" << endl;
+    graph.deleteEdge("2792", "2796");
+    graph.displayVertex("2792");
+    graph.displayVertex("2796");
+    graph.display();
+    // delete vertex
+    cout << "Deleting vertex" << endl;
+    graph.deleteVertex("2792");
+    graph.displayVertex("2792");
+    graph.displayVertex("2796");
+    graph.display();
+    // clear graph
+    cout << "Clearing graph" << endl;
+    graph.clear();
+    graph.display();
+}
+
 void test_kruskal() {
     
     Graph<string,float> graph_kruskal;
